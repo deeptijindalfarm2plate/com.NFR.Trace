932415a83ea4c176d2d57fc644085a3afae628d5
deeptijindalfarm2plate <deepti.jindal@farmtoplate.io>
2024-04-26 17:19:12 +0530

    New code to demo

---
+++ src/main/java/com/demo/baseutil/Initializepages.java
@@ -0,0 +1,57 @@
+package com.demo.baseutil;
+
+import org.openqa.selenium.WebDriver;
+
+import com.demo.pages.Tracelogin;
+import com.demo.pages.Tracelogin2;
+import com.demo.pages.Tracesigninpage;
+import com.demo.pages.Tracesignuppage;
+import com.demo.pages.Tracetransaction;
+import com.demo.pages.Tracelogout;
+import com.demo.pages.Tracenetworkmgmt;
+import com.demo.pages.Traceprocess;
+import com.demo.pages.Traceproduct;
+import com.demo.pages.Traceasset;
+import com.demo.pages.Tracedashboard;
+import com.demo.pages.Traceinfra;
+import com.demo.util.WebActionUtil;
+import com.demo.mail.Gmail;
+
+/**
+ * Description: Initializes all pages with driver instance ,Explicit Time out,
+ * WebAactionUtility using variables driver,ETO,WebactionUtil
+ *
+ * @author : Deepti jindal
+ */
+public class Initializepages extends Basetest2 {
+
+	public Tracesigninpage signinpage;
+	public Tracelogin loginpage;
+	public Tracelogout logout;
+	public Tracelogin2 loginpage2;
+	public Tracesignuppage signup;
+	public Tracedashboard modules;
+	public Tracenetworkmgmt nwmgmt;
+	public Traceasset assetpage;
+	public Traceinfra infrapage;
+	public Traceproduct products;
+	public Traceprocess process;
+	public Tracetransaction transaction;
+	
+	public Initializepages(WebDriver driver, long eto, WebActionUtil actionutil) {
+
+		signinpage = new Tracesigninpage(driver, eto, actionutil);
+		loginpage = new Tracelogin(driver, eto, actionutil);
+		loginpage2 = new Tracelogin2(driver, eto, actionutil);
+		logout = new Tracelogout(driver, eto, actionutil);
+		signup= new Tracesignuppage(driver, eto,actionutil);
+		modules= new Tracedashboard(driver, eto, actionutil);
+		nwmgmt = new Tracenetworkmgmt(driver, eto, actionutil);
+		assetpage= new Traceasset(driver, eto,actionutil);
+		infrapage= new Traceinfra(driver, eto,actionutil);
+		products = new Traceproduct(driver, eto,actionutil);
+		process = new Traceprocess(driver, eto,actionutil);
+		transaction = new Tracetransaction(driver, eto,actionutil);
+	}
+
+}
---
+++ src/main/java/com/demo/pages/Tracelogin.java
@@ -0,0 +1,100 @@
+package com.demo.pages;
+
+import static org.testng.Assert.assertEquals;
+
+import java.io.FileInputStream;
+import java.util.Properties;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import com.demo.baseutil.Basetest;
+import com.demo.util.WebActionUtil;
+
+public class Tracelogin {
+	public WebDriver driver;
+	public WebActionUtil actionutil;
+	public static Properties prop = new Properties();
+	public long eto = 30;
+	public static final String usrdir = "user.dir";
+	public static final String CONFIGPATH = System.getProperty(usrdir) + "./config/config.properties";
+
+	public Tracelogin(WebDriver driver, long eto, WebActionUtil actionutil) {
+		this.driver = driver;
+		PageFactory.initElements(driver, this);
+		this.actionutil = actionutil;
+		this.eto = eto;
+	}
+	static {
+		try {
+			prop = new Properties();
+			FileInputStream fis = new FileInputStream(CONFIGPATH);
+			prop.load(fis);
+		} catch (Exception e) {
+			e.printStackTrace();
+		}
+
+	}
+
+
+	/* X-path for email */
+	@FindBy(xpath = "//input[@id = 'username_login_input']")
+	private WebElement email;
+
+	/* X-path for password */
+	@FindBy(xpath = "//input[@id = 'password_login_input']")
+	private WebElement pass;
+
+	/* X-path for Signin button */
+	@FindBy(xpath = "//button[@id = 'login_submit_button']")
+	private WebElement signin;
+	
+	/* X-path for Supplier button */
+	@FindBy(xpath = "(//button[@id = 'header_user_dropdown'])[3]")
+	private WebElement proflie;
+	
+	@FindBy(xpath = "//a[@id = 'Logout_header_select']")
+	private WebElement signout;
+
+	/**
+	 * Description: Clicking on Sign In button and verify the UI of the Sign-in
+	 * page.
+	 * @throws InterruptedException 
+	 */
+	public void verifyLogin(String emailfield, String passwordfield) throws InterruptedException {
+		actionutil.info("Sign in function call");
+		actionutil.clickOnElement(email, "email address field is clicked");
+		actionutil.typeText(email, emailfield, "Email Id Entered");
+		actionutil.clickOnElement(pass, "Enter the password");
+		actionutil.typeText(pass, passwordfield, "Password Id Entered");
+		actionutil.scrollDown();
+		actionutil.clickOnElement(signin, "Press the signin button");
+		Thread.sleep(2000);
+		String siginPageTitle = driver.getTitle();
+		//System.out.println("print the value of siginPageTitle" + siginPageTitle );
+		assertEquals(siginPageTitle, Basetest.prop.getProperty("HomePageTitle"));
+		Basetest.logger.info("HomePage title  is matched");
+		Thread.sleep(2000);		
+		return;
+	}
+	
+	public void verifysuperadminLogin() throws InterruptedException {
+		actionutil.info("Sign in function call");
+		String emailfield = prop.getProperty("emailfield");
+		String passwordfield = prop.getProperty("passwordfield");
+		actionutil.clickOnElement(email, "email address field is clicked");
+		actionutil.typeText(email, emailfield, "Email Id Entered");
+		actionutil.clickOnElement(pass, "Enter the password");
+		actionutil.typeText(pass, passwordfield, "Password Id Entered");
+		actionutil.scrollDown();
+		actionutil.clickOnElement(signin, "Press the signin button");
+		Thread.sleep(8000);	
+		String siginPageTitle = driver.getTitle();
+		assertEquals(siginPageTitle, Basetest.prop.getProperty("HomePageTitle"));
+		Basetest.logger.info("HomePage title  is matched");
+		Thread.sleep(8000);		
+		return;
+	}
+}
